6fcabf1a56b6bd7bc25d2d125b7b7fa580f81851
Hector Nevado <hectornevado46@gmail.com>
2022-07-02 19:17:37 +0200

    Añadida protección de las llamadas a API v1 y API v2

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V2\PostController as PostV2;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,8 +18,15 @@ use App\Http\Controllers\Api\V2\PostController as PostV2;
 
 //API V1
 Route::apiResource('v1/posts', PostV1::class)
-->only(['index','show','destroy']);
+->only(['index','show','destroy'])
+->middleware('auth:sanctum');
 
 //API V2
 Route::apiResource('v2/posts', PostV2::class)
-->only(['index','show']);
+->only(['index','show'])
+->middleware('auth:sanctum');
+
+Route::post('login', [
+    App\Http\Controllers\Api\LoginController::class,
+    'login'
+]);
